04b941f16755776d6fdf401fb66f0e5365e90838
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2018-01-24 22:23:00 -0200

    Slider ended

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
 {
-    console.log('Hello world!')
+  const DOMstrings = {
+    slider: '.slider-js',
+    sliderBtn: '.slider__buttons'
+  };
+
+  $(DOMstrings.slider).slick({
+    infinite: true,
+    appendArrows: $(DOMstrings.sliderBtn),
+    responsive: [{
+        breakpoint: 992,
+        settings: {
+            infinite: true,
+            dots: true,
+            arrows: false
+        }
+    }]
+  });
 }
