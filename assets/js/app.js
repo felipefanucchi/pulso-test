7a7ee21192446b38a5b66e75c6a889e69a647b66
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2018-01-25 18:21:07 -0200

    finished the test

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,42 @@
-{
+function init() {
   const DOMstrings = {
     slider: '.slider-js',
-    sliderBtn: '.slider__buttons'
+    sliderBtn: '.slider__buttons',
+    sectionBg: '.section--background-js',
+    header: '.header',
+    headerWrapper: '.header__wrapper-js'
   };
+
+  const breakpoints = {
+    md: 992
+  }
+
+  const sectionBg = () => {
+    const img = $(DOMstrings.sectionBg).data('image');
+
+    $(DOMstrings.sectionBg).css('background-image', `url(${img})`);
+  }
+
+  const sticky = (e) => {
+    const headerHeight = $(DOMstrings.headerWrapper).height();
+    const scrollTop = $(window).scrollTop();
+
+    console.log(scrollTop, headerHeight);
+    
+    if(scrollTop > headerHeight) {
+      $(DOMstrings.header).addClass('header__sticky');
+    } else {
+      $(DOMstrings.header).removeClass('header__sticky');
+    }
+  }
+
+  $(window).on('scroll', sticky);
 
   $(DOMstrings.slider).slick({
     infinite: true,
     appendArrows: $(DOMstrings.sliderBtn),
     responsive: [{
-        breakpoint: 992,
+      breakpoint: breakpoints.md,
         settings: {
             infinite: true,
             dots: true,
@@ -16,4 +44,8 @@
         }
     }]
   });
+
+  sectionBg();
 }
+
+init();
